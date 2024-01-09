2792ef6a6c2e43be64d27d11c1e745f42d3b69bf
Justice <ugboguj@yahoo.com>
2024-01-09 01:42:44 +0100

    Added Request for parent registration

--- app/Http/Controllers/Parent/ParentAuthController.php
+++ app/Http/Controllers/Parent/ParentAuthController.php
@@ -3,71 +3,35 @@
 namespace App\Http\Controllers\Parent;
 
 use App\Models\User;
+use App\Helpers\Lesson;
 use App\Models\Learner;
 use App\Models\Lessons;
 use App\Models\ParentUser;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\LessonLearner;
 use App\Models\LessonSubject;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\LessonLearner;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Models\LessonSubjectTimetable;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
-use App\Helpers\Lesson;
+use App\Http\Requests\ParentController\SignUpRequest;
 
 
 class ParentAuthController extends Controller
 {
     //
-    public function signup(Request $request){
+    public function signup(SignUpRequest $request){
+        $request->validated();
 
         if(auth()->user()){
             auth()->user()->tokens()->delete();
         }
-
-        $fields = Validator::make($request->all(), [
-            
-            'email' => 'required|string|email',
-            'firstname' => 'required|string',
-            'lastname' => 'required|string',
-            'state' => 'required|string',
-            'country' => 'required|string',
-            'address' => 'required|string',
-            'phone' => 'required|string',
-            'title' => 'required|string',
-            'gender' => 'required|string',
-            
-            'how_did_you_know_about_us' => 'required|string',
-
-            'learners_name' => 'required|string',
-            'learners_dob' => 'required|string',
-            'learners_gender' => 'required|string',
-
-            'lesson_address' => 'required|string',
-            'lesson_goals' => 'required|string',
-            'lesson_mode' => 'required|string',
-            'lesson_period' => 'required|string',
-            'description_of_learner' => 'required|string',
-            'lesson_commence' => 'required|string',
-            'education_level_id' => 'required|string|exists:education_levels,id',
-           
-            'total_subjects' => 'required|integer',
-            'api_key' => 'required|string',
-        ]); // request body validation rules
-
-        if($fields->fails()){
-            return response()->json([
-                'status_code' => Response::HTTP_UNPROCESSABLE_ENTITY,
-                'status' => 'error',
-                'message' => $fields->messages(),
-            ], Response::HTTP_UNPROCESSABLE_ENTITY); // 422
-        } // request body validation failed, so lets return
-
+        
         for ($i_subjects=1; $i_subjects <= $request->total_subjects; $i_subjects++) {
 
             $fields = Validator::make($request->all(), [
